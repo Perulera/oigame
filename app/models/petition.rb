518382ac78e920eeecb5378c969a854ec9fd34c4
Antonio Pardo <apardo@alabs.es>
2012-09-14 13:01:36 +0200

    validates presence of email

--- app/models/petition.rb
+++ app/models/petition.rb
@@ -6,6 +6,8 @@ class Petition < ActiveRecord::Base
 
   scope :validated, where(:validated => true)
 
+  validates_presence_of :email
+
   # generar método que valie tu un remitente no puede unirse
   # más de una vez a la campaña
   validates_uniqueness_of :email, :scope => :campaign_id
